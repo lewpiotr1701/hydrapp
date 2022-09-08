cd93b0a033ebb2b3e0023b0c65845989bdb66894
Piotr Lewczyk <lewczykpiotr@gmail.com>
2022-09-08 08:29:55 +0200

    initial version of hydrapp

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,34 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const glassesCounter = document.querySelector('.glasses--js');
+const addButton = document.querySelector('.add--js');
+const deleteButton = document.querySelector('.delete--js');
+
+let key = new Date().toISOString().slice(0, 10);
+let currentValue = localStorage.getItem(key);
+ 
+// Check every time the page is refreshed
+if (currentValue === null) {
+    localStorage.setItem(key, '0');
+    glassesCounter.innerHTML = '0';
+} else {
+    glassesCounter.innerHTML = currentValue;
+}
+
+
+addButton.addEventListener('click', () => {
+    currentValue = parseInt(localStorage.getItem(key));
+    currentValue += 1;
+    localStorage.setItem(key, currentValue.toString());
+    glassesCounter.innerHTML = currentValue;
+})
+
+deleteButton.addEventListener('click', () => {
+    currentValue = parseInt(localStorage.getItem(key));
+    if (currentValue != 0) {
+        currentValue -= 1;
+        localStorage.setItem(key, currentValue.toString());
+        glassesCounter.innerHTML = currentValue;
+    }
+})
